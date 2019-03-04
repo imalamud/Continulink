8b2027c5d87f8dc7e5f8a035cabc2b7c114c8628
Mobin Masudi <nmasudi@compliahealth.com>
2019-03-04 15:09:22 -0600

    CLNK-23505

--- Scripts/Regression BackOffice (All Browsers)/CLNK-23505/Script1551732437811.groovy
+++ Scripts/Regression BackOffice (All Browsers)/CLNK-23505/Script1551732437811.groovy
@@ -13,7 +13,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('')
+WebUI.openBrowser('WWW.google.com')
 
 WebUI.maximizeWindow()
 
