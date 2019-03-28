44406b06831ed8ef0bab61b38e2d631175a4e9bb
Isaac <isaac.malamud@mqs.com.uy>
2019-03-28 09:10:10 -0300

    CLNK-23504 Created

---
+++ Object Repository/Regression Clinical Edge/CLINK - 23504/Plan of Care Section.rs
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Plan of Care Section</name>
+   <tag></tag>
+   <elementGuidId>1afbf831-7ead-41cc-9457-55e9c894eec6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='client_left-col']/div[3]/h3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>pageTitle</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Plan of Care</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;client_left-col&quot;)/div[@class=&quot;homePageWidget cw-03&quot;]/h3[@class=&quot;pageTitle&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//div[@id='client_left-col']/div[3]/h3</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='No blocked days or hours found for this client.'])[1]/following::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Remarks'])[1]/following::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Certification Periods and POCs'])[1]/preceding::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div[3]/h3</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/Regression Clinical Edge (All Browsers)/CLNK-23504 - Clinical Edge - POC - Pick Assessment/Script1553773501310.groovy
@@ -0,0 +1,61 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.callTestCase(findTestCase('Regression Clinical Edge (All Browsers)/CLNK-23349 - ContinuLink - Login'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.waitForPageLoad(60)
+
+WebUI.maximizeWindow()
+
+WebUI.waitForPageLoad(60)
+
+WebUI.delay(8)
+
+WebUI.waitForElementPresent(findTestObject('Regression Clinical Edge/CLNK-23497/button_Close'), 100)
+
+WebUI.click(findTestObject('Regression Clinical Edge/CLNK-23497/button_Close'))
+
+WebUI.delay(2)
+
+WebUI.waitForElementPresent(findTestObject('Regression Clinical Edge/HomePage/ClientLink'), 100)
+
+WebUI.click(findTestObject('Regression Clinical Edge/HomePage/ClientLink'))
+
+WebUI.delay(2)
+
+WebUI.waitForPageLoad(50)
+
+WebUI.waitForElementPresent(findTestObject('Regression Clinical Edge/CLNK-23497/Client Search'), 50)
+
+WebUI.click(findTestObject('Regression Clinical Edge/CLNK-23497/Client Search'))
+
+WebUI.setText(findTestObject('Regression Clinical Edge/CLNK-23497/Client Search'), GlobalVariable.G_Client)
+
+WebUI.delay(2)
+
+WebUI.sendKeys(findTestObject('Regression Clinical Edge/CLNK-23497/Client Search'), Keys.chord(Keys.ARROW_RIGHT, Keys.ARROW_DOWN, 
+        Keys.ENTER))
+
+WebUI.delay(3)
+
+WebUI.scrollToElement(findTestObject('Regression Clinical Edge/CLINK - 23504/Plan of Care Section'), 50)
+
+WebUI.waitForElementPresent(findTestObject('Regression Clinical Edge/CLINK - 23504/a_Plan of Care'), 50)
+
+WebUI.click(findTestObject('Regression Clinical Edge/CLINK - 23504/a_Plan of Care'))
+
+WebUI.delay(3)
+
